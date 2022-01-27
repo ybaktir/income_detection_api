be68efaf107c850856c441cd1250fe1d3c2d59d2
Yusuf Baktir, Ph.D <33700432+ybaktir@users.noreply.github.com>
2022-01-26 21:26:45 -0600

    fixing the ip

--- app/main.py
+++ app/main.py
@@ -52,4 +52,5 @@ def get_person(data: Person):
 
 
 if __name__ == '__main__':
-    uvicorn.run(app, host='0.0.0.0', port=80, debug=True)
+    #uvicorn.run(app, host='127.0.0.1', port=8000, debug=True)
+    uvicorn.run(app, host='0.0.0.0', port=80)
